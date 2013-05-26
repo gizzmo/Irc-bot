93df885ea8872ccefb32b30b76b49a8efb41235c
Gizzmo <justgiz@gmail.com>
2013-05-25 17:56:02 -0700

    Update dummy plugin

--- plugins/dummy.js
+++ plugins/dummy.js
@@ -9,18 +9,24 @@ Plugin = exports.Plugin = function(irc, name) {
 
 	this.title = 'Dummy Plugin';
 	this.version = '0.1';
-	this.author = 'Markus M. May';
+
+	// Triggers are messages that start with `!`
+	this.irc.addTrigger(this, 'trigger', this.trigTrigger);
 };
 
 util.inherits(Plugin, basePlugin.BasePlugin)
 
-Plugin.prototype.onNumeric = function(msg) {
-	var command = msg.rawCommand;
+Plugin.prototype.trigTrigger = function(msg) {};
 
-	// 376 is end of MOTD/modes
-	if (command !== '376') {
-		return false;
-	}
-
-	return true;
-};
+/**
+ * List all possible Event Listeners
+Plugin.prototype.onConnect = function(msg) {};
+Plugin.prototype.onData = function(msg) {};
+Plugin.prototype.onNumeric = function(msg) {};
+Plugin.prototype.onMessage = function(msg) {};
+Plugin.prototype.onJoin = function(msg) {};
+Plugin.prototype.onPart = function(msg) {};
+Plugin.prototype.onQuit = function(msg) {};
+Plugin.prototype.onNick = function(msg) {};
+Plugin.prototype.onPrivateMessage = function(msg) {};
+ */
