3838312290d2bc863e096896706ca11d848e6cc8
Gizzmo <justgiz@gmail.com>
2013-05-26 17:11:55 -0700

    simple knock knock joke plugin

---
+++ plugins/knock_knock.js
@@ -0,0 +1,69 @@
+/**
+ * Know Knock joke Plugin
+ */
+var util = require('util'),
+	basePlugin = require('./basePlugin');
+
+Plugin = exports.Plugin = function(irc, name) {
+	Plugin.super_.call(this, irc, name);
+
+	this.title = 'Knock Knock Plugin';
+	this.version = '0.1';
+
+	// Triggers are messages that start with `!`
+	this.irc.addTrigger(this, 'joke', this.trigJoke);
+
+	// current joke progress
+	this.progress = 0;
+};
+
+util.inherits(Plugin, basePlugin.BasePlugin)
+
+Plugin.prototype.trigJoke = function(msg) {
+	var irc = this.irc,
+		user = irc.users.find(msg.nick), // the user who send the message
+		chan = irc.channels.find(msg.arguments[0]), // the channel it was sent to
+		m = msg.arguments[1]; // the message its self
+
+	if (this.progress === 0){
+		++this.progress;
+		return chan.send('Ok, i\'ve got a joke for you. Knock Knock!');
+	}
+	else {
+		return chan.send('I\'m already telling a joke, '+ user.nick);
+	}
+};
+
+Plugin.prototype.onMessage = function(msg) {
+	var irc = this.irc,
+		user = irc.users.find(msg.nick), // the user who send the message
+		chan = irc.channels.find(msg.arguments[0]), // the channel it was sent to
+		m = msg.arguments[1]; // the message its self
+
+	if (this.progress === 1) {
+		if ( m.match(/^who(\'?s| is) there/i) ) {
+			++this.progress;
+
+			return chan.send('Doris!');
+		}
+	}
+	else if (this.progress == 2) {
+		if ( m.match(/doris who/i) ) {
+			this.progress = 0;
+
+			return chan.send('Doris locked, that\'s why im knocking!');
+		};
+	}
+};
+/**
+ * List all possible Event Listeners
+Plugin.prototype.onConnect = function(msg) {};
+Plugin.prototype.onData = function(msg) {};
+Plugin.prototype.onNumeric = function(msg) {};
+Plugin.prototype.onMessage = function(msg) {};
+Plugin.prototype.onJoin = function(msg) {};
+Plugin.prototype.onPart = function(msg) {};
+Plugin.prototype.onQuit = function(msg) {};
+Plugin.prototype.onNick = function(msg) {};
+Plugin.prototype.onPrivateMessage = function(msg) {};
+ */
---
+++ test/knock_knock.spec.js
@@ -0,0 +1,94 @@
+var irc  = require('./irc-stub.js'),
+	message = require('../lib/message'),
+	knock_knock = require('../plugins/knock_knock.js'),
+	should = require('should');
+
+describe("KnockKnock", function(){
+	var config = {};
+	var _irc, _knock_knock;
+
+	beforeEach(function() {
+		_irc = new irc.Irc(config);
+		_knock_knock = new knock_knock.Plugin(_irc, 'Knock_Knock');
+	});
+
+	it('should start one joke process on request', function() {
+		var test = new message.Message(':stubUser!~stubUser@users.ircserver.org PRIVMSG #stubChannel :!joke');
+		var result = 'PRIVMSG #stubChannel :Ok, i\'ve got a joke for you. Knock Knock!';
+
+		var call = _knock_knock.trigJoke(test);
+		var resultMessage = _irc.resultMessage;
+
+		JSON.stringify(resultMessage).should.equal(JSON.stringify(result));
+		_knock_knock.progress.should.equal(1);
+
+		// if somone else
+		var result = 'PRIVMSG #stubChannel :I\'m already telling a joke, stubUser';
+
+		var call = _knock_knock.trigJoke(test);
+		var resultMessage = _irc.resultMessage;
+		JSON.stringify(resultMessage).should.equal(JSON.stringify(result));
+
+	})
+	it('should responde to correct msg', function() {
+
+		// possible stage 1 respones
+		['whos there', 'who\'s there','whos there?','who\'s there?','who is there','who is there?',
+		'Whos There', 'Who\'s There','Whos There?','Who\'s There?','Who is There','Who is There?'].forEach(function(msg) {
+			// reset the progress to test each possible result
+			_knock_knock.progress = 1;
+
+			var test = new message.Message(':stubUser!~stubUser@users.ircserver.org PRIVMSG #stubChannel :'+msg);
+			var result = 'PRIVMSG #stubChannel :Doris!';
+
+			var call = _knock_knock.onMessage(test);
+			var resultMessage = _irc.resultMessage;
+			JSON.stringify(resultMessage).should.equal(JSON.stringify(result));
+		});
+
+		// stage two
+		['doris who', 'doris who?'].forEach(function(msg) {
+			// reset the progress to test each possible result
+			_knock_knock.progress = 2;
+
+			var test = new message.Message(':stubUser!~stubUser@users.ircserver.org PRIVMSG #stubChannel :'+msg);
+			var result = 'PRIVMSG #stubChannel :Doris locked, that\'s why im knocking!';
+
+			var call = _knock_knock.onMessage(test);
+			var resultMessage = _irc.resultMessage;
+			JSON.stringify(resultMessage).should.equal(JSON.stringify(result));
+		});
+
+	})
+	it('shouldnt respond if msg doesnt match progress', function() {
+
+		['who\'s that?', 'who is that?', 'whats there', 'whats this', 'ok, whos there']. forEach(function(msg) {
+			// reset progress
+			_knock_knock.progress = 1;
+
+			// test invalid responce
+			var test = new message.Message(':stubUser!~stubUser@users.ircserver.org PRIVMSG #stubChannel :'+ msg);
+			var result = 'NOTHING HAPPENED';
+
+			var call = _knock_knock.onMessage(test);
+			var resultMessage = _irc.resultMessage;
+			JSON.stringify(resultMessage).should.equal(JSON.stringify(result));
+
+			_knock_knock.progress.should.equal(1);
+		})
+	})
+
+
+
+
+	it('should have a name', function() {
+		JSON.stringify(_knock_knock.name).should.equal(JSON.stringify('Knock_Knock'));
+	}),
+	it('should have a version', function() {
+		JSON.stringify(_knock_knock.version).should.not.equal(JSON.stringify('undefined'));
+	}),
+	it('should have a title', function() {
+		JSON.stringify(_knock_knock.title).should.not.equal(JSON.stringify('undefined'));
+	})
+
+});
